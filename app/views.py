ce771d85373e165d40e73bc9a9fb83e2327e193e
bigjillja <officialmartinbartley@gmail.com>
2023-02-12 22:40:19 -0500

    Final Commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 
 ###
@@ -16,6 +17,15 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    """Render the website's profile page"""
+    return render_template('profile.html')
+
+def format_date_joined(date):
+    datejoined = datetime.strptime(date, '%Y-%m-%d')
+    return datejoined.strftime('%B, %Y')
 
 
 ###
